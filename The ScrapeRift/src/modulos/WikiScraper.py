64fd19411f621db05f41edf8e2470f53f8647ad1
Alessandro Garzon Melo <algarzonme@unal.edu.co>
2025-03-04 14:47:38 -0500

    Update WikiScraper.py
    Solución para que scrapee bien la info así se ponga el nombre del campeon sin la primera letra en mayuscula

--- The ScrapeRift/src/modulos/WikiScraper.py
+++ The ScrapeRift/src/modulos/WikiScraper.py
@@ -11,7 +11,7 @@ import time
 class LolScraper(BaseScraper):
     def __init__(self, base_url='https://wiki.leagueoflegends.com/en-us/', driver= None, champion = None):
         super().__init__(base_url, driver)
-        self.champion = champion
+        self.champion = champion.capitalize()
 
     #* Extract the champion names from the League of Legends wiki
     def extract_champion_names(self):
@@ -21,11 +21,7 @@ class LolScraper(BaseScraper):
             WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, '[data-champion]'))
             )
-            
-            # Find the elements with the data-champion attribute
             champion_elements = self.driver.find_elements(By.CSS_SELECTOR, '[data-champion]')
-
-            # Extract the data-champion attribute from each element
             champion_names = [element.get_attribute("data-champion") for element in champion_elements]
             sorted_champion_names = sorted(champion_names)
         except Exception as e:
@@ -34,23 +30,23 @@ class LolScraper(BaseScraper):
         return sorted_champion_names
     
     #* Extract the health of the champion
-    def extract_champion_health(self,champion):
+    def extract_champion_health(self):
         try:
             WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, f'//*[@id="Health_{champion}"]'))
+                EC.presence_of_element_located((By.XPATH, f'//*[@id="Health_{self.champion}"]'))
             )
-            health = self.driver.find_element(By.XPATH, f'//*[@id="Health_{champion}"]')
+            health = self.driver.find_element(By.XPATH, f'//*[@id="Health_{self.champion}"]')
             return health.text if health else None
         except Exception as e:
             print(f"Error at extracting the health of the champion: {e}")
     
     #* Extract the mana of the champion    
-    def extract_champion_mana(self,champion):
+    def extract_champion_mana(self):
         try:
             WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, f'//*[@id="ResourceBar_{champion}"]'))
+                EC.presence_of_element_located((By.XPATH, f'//*[@id="ResourceBar_{self.champion}"]'))
             )
-            mana = self.driver.find_element(By.XPATH, f'//*[@id="ResourceBar_{champion}"]')
+            mana = self.driver.find_element(By.XPATH, f'//*[@id="ResourceBar_{self.champion}"]')
             return mana.text if mana else 'El campeon no usa mana'
         except Exception as e:
             print(f"Error at extracting the mana of the champion: {e}")
@@ -58,69 +54,68 @@ class LolScraper(BaseScraper):
         
     
     #* Extract the health regen of the champion
-    def extract_champion_health_regen(self, champion = None):
+    def extract_champion_health_regen(self):
         try:
             WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, f'//*[@id="HealthRegen_{champion}"]'))
+                EC.presence_of_element_located((By.XPATH, f'//*[@id="HealthRegen_{self.champion}"]'))
             )
-            health_regen = self.driver.find_element(By.XPATH, f'//*[@id="HealthRegen_{champion}"]')
+            health_regen = self.driver.find_element(By.XPATH, f'//*[@id="HealthRegen_{self.champion}"]')
             return health_regen.text if health_regen else None
         except Exception as e:
             print(f"Error at extracting the health of the champion: {e}")
          
     
     #* Extract the mana regen of the champion
-    def extract_champion_mana_regen(self, champion = None):
+    def extract_champion_mana_regen(self):
         try:
             WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, f'//*[@id="ResourceRegen_{champion}"]'))
+                EC.presence_of_element_located((By.XPATH, f'//*[@id="ResourceRegen_{self.champion}"]'))
             )
-            mana_regen = self.driver.find_element(By.XPATH, f'//*[@id="ResourceRegen_{champion}"]')
+            mana_regen = self.driver.find_element(By.XPATH, f'//*[@id="ResourceRegen_{self.champion}"]')
             return mana_regen.text if mana_regen else "El campeon no usa mana"
         except Exception as e:
             print(f"Error at extracting the mana of the champion: {e}")
          
 
     #* Extract the armor of the champion
-    def extract_champion_armor(self, champion = None):
+    def extract_champion_armor(self):
         try:
             WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, f'//*[@id="Armor_{champion}"]'))
+                EC.presence_of_element_located((By.XPATH, f'//*[@id="Armor_{self.champion}"]'))
             )
-            armor = self.driver.find_element(By.XPATH, f'//*[@id="Armor_{champion}"]')
+            armor = self.driver.find_element(By.XPATH, f'//*[@id="Armor_{self.champion}"]')
             return armor.text if armor else None
         except Exception as e:
             print(f"Error at extracting the armor of the champion: {e}")
             
     
     #* Extract the attack damage of the champion
-    def extract_champion_attack_damage(self, champion = None):
+    def extract_champion_attack_damage(self):
         try:
             WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, f'//*[@id="AttackDamage_{champion}"]'))
+                EC.presence_of_element_located((By.XPATH, f'//*[@id="AttackDamage_{self.champion}"]'))
             )
-            attack_damage = self.driver.find_element(By.XPATH, f'//*[@id="AttackDamage_{champion}"]')
+            attack_damage = self.driver.find_element(By.XPATH, f'//*[@id="AttackDamage_{self.champion}"]')
             return attack_damage.text if attack_damage else None
         except Exception as e:
             print(f"Error at extracting the attack damage of the champion: {e}")
         
     
     #* Extract the magic resist of the champion
-    def extract_champion_magic_resist(self, champion = None):
+    def extract_champion_magic_resist(self):
         try:
             WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, f'//*[@id="MagicResist_{champion}"]'))
+                EC.presence_of_element_located((By.XPATH, f'//*[@id="MagicResist_{self.champion}"]'))
             )
-            magic_resist = self.driver.find_element(By.XPATH, f'//*[@id="MagicResist_{champion}"]')
+            magic_resist = self.driver.find_element(By.XPATH, f'//*[@id="MagicResist_{self.champion}"]')
             return magic_resist.text if magic_resist else None
         except Exception as e:
             print(f"Error at extracting the magic resist of the champion: {e}")
         
     
     #* Extract the crit damage of the champion
-    def extract_champion_crit_damage(self, champion=None):
+    def extract_champion_crit_damage(self):
         try:
-            # Usa el XPath correcto según lo que inspecciones en el navegador.
             xpath_crit = '/html/body/div[4]/div[3]/div[5]/div[1]/div[5]/div[2]/div[8]/div[2]'
             WebDriverWait(self.driver, 10).until(
                 EC.visibility_of_element_located((By.XPATH, xpath_crit))
@@ -132,24 +127,24 @@ class LolScraper(BaseScraper):
         
     
     #* Extract the move speed of the champion
-    def excact_move_speed(self, champion = None):
+    def excact_move_speed(self):
         try:
             WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.ID,f"MovementSpeed_{champion}"))
+                EC.presence_of_element_located((By.ID,f"MovementSpeed_{self.champion}"))
             )
-            move_speed = self.driver.find_element(By.ID, f"MovementSpeed_{champion}")
+            move_speed = self.driver.find_element(By.ID, f"MovementSpeed_{self.champion}")
             return move_speed.text if move_speed else None
         except Exception as e:
             print(f"Error at extracting the move speed of the champion: {e}")
         
     
     #* Extract the range of the champion
-    def extract_champion_range(self, champion = None):
+    def extract_champion_range(self):
         try:
             WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.ID,f"AttackRange_{champion}"))
+                EC.presence_of_element_located((By.ID,f"AttackRange_{self.champion}"))
             )
-            range = self.driver.find_element(By.ID, f"AttackRange_{champion}")
+            range = self.driver.find_element(By.ID, f"AttackRange_{self.champion}")
             return range.text if range else None
         except Exception as e:
             print(f"Error at extracting the range of the champion: {e}")
@@ -166,17 +161,17 @@ class LolScraper(BaseScraper):
     
     #* Scrape all the information of the champion
     def scrape(self):
-        time.sleep(2)
-        health = self.extract_champion_health(self.champion)
-        mana = self.extract_champion_mana(self.champion)
-        health_regen = self.extract_champion_health_regen(self.champion)
-        mana_regen = self.extract_champion_mana_regen(self.champion)
-        armor = self.extract_champion_armor(self.champion)
-        attack_damage = self.extract_champion_attack_damage(self.champion)
-        magic_resist = self.extract_champion_magic_resist(self.champion)
-        crit_damage = self.extract_champion_crit_damage(self.champion)
-        move_speed = self.excact_move_speed(self.champion)
-        range = self.extract_champion_range(self.champion)
+        time.sleep(1)
+        health = self.extract_champion_health()
+        mana = self.extract_champion_mana()
+        health_regen = self.extract_champion_health_regen()
+        mana_regen = self.extract_champion_mana_regen()
+        armor = self.extract_champion_armor()
+        attack_damage = self.extract_champion_attack_damage()
+        magic_resist = self.extract_champion_magic_resist()
+        crit_damage = self.extract_champion_crit_damage()
+        move_speed = self.excact_move_speed()
+        range = self.extract_champion_range()
         
         dictionary = {
             'Champion': self.champion,
